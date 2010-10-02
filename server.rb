8aaf7824245739cac16c8fe852851e67a676659e
opsb <oliver@opsb.co.uk>
2010-10-02 17:57:47 +0100

    Initial commit

---
+++ server.rb
@@ -0,0 +1,11 @@
+require 'rubygems'
+require 'sinatra'
+require 'json'
+
+use Rack::Auth::Basic do |username, password|
+  username == 'heroku' && password == 'b1EWrHYXE1R5J71D'
+end
+
+post '/heroku/resources' do
+  { :id => 1 }.to_json
+end
