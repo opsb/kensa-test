3cb3a6df1e3e9dd127673c55d42ac0d220655da1
opsb <oliver@opsb.co.uk>
2010-10-02 19:44:02 +0100

    args

--- server.rb
+++ server.rb
@@ -7,9 +7,7 @@ use Rack::Auth::Basic do |username, password|
 end
 
 post '/heroku/resources' do
-  vars = { :id => 1, "KP_HOME_PAGE" => "http://google.com" }.to_json
-  puts vars.inspect
-  vars
+  { :id => 1, "KP_HOME_PAGE" => "http://google.com" }.to_json
 end
 
 delete '/heroku/resources/:id' do
